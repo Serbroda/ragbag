9a1ee8c139135b09606b74f56c4f17bf092c0525
Danny Rottstegge <danny@rottstegge.net>
2020-07-04 01:53:42 +0200

    Add terrariabackup.py

---
+++ scripts/python/terrariabackup.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import pickle
+import time
+import zipfile
+import os
+import hashlib
+import getopt, sys
+
+# https://stackoverflow.com/questions/27913261/python-storing-data
+# https://www.geeksforgeeks.org/working-zip-files-python/
+# https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
+
+ARGS_KEY_CWD = 'cwd'
+ARGS_KEY_INPUT = 'input'
+ARGS_KEY_OUTPUT = 'output'
+ARGS_KEY_DATA_FILE = 'data'
+
+DEFAULT_DATA_FILE = 'terrariabackup.data'
+DATA_KEY_LAST_FHASH = 'last_fhash'
+DATA_KEY_LAST_BACKUP_FILE = 'last_backup_file'
+DATA_KEY_LAST_FILE_COUNT = 'last_file_count'
+
+
+def get_args():
+    args = {}
+    args[ARGS_KEY_DATA_FILE] = DEFAULT_DATA_FILE
+
+    # Get full command-line arguments
+    full_cmd_arguments = sys.argv
+
+    # Keep all but the first
+    argument_list = full_cmd_arguments[1:]
+
+    short_options = "hi:o:c:d:"
+    long_options = ["help", ARGS_KEY_INPUT + "=", ARGS_KEY_OUTPUT + "=", ARGS_KEY_CWD + "=", ARGS_KEY_DATA_FILE + "="]
+
+    arguments, values = getopt.getopt(argument_list, short_options, long_options)
+
+    for current_argument, current_value in arguments:
+        if current_argument in ("-h", "--help"):
+            print ("Command line argument list:")
+            print ("")
+            print ("------------------------------------------")
+            print ("long argument   short argument  with value")
+            print ("------------------------------------------")
+            print ("--help           -h              no")
+            print ("--input          -o              yes")
+            print ("--output         -o              yes")
+            print ("--cwd            -c              yes")
+            print ("------------------------------------------")
+            exit(2)
+        elif current_argument in ("-i", "--" + ARGS_KEY_INPUT):
+            args[ARGS_KEY_INPUT] = current_value.split(',')
+        elif current_argument in ("-o", "--" + ARGS_KEY_OUTPUT):
+            args[ARGS_KEY_OUTPUT] = current_value
+        elif current_argument in ("-d", "--" + ARGS_KEY_DATA_FILE):
+            args[ARGS_KEY_DATA_FILE] = current_value
+        elif current_argument in ("-c", "--" + ARGS_KEY_CWD):
+            try:
+                os.chdir(current_value)
+            except OSError:
+                print("Can't change the Current Working Directory to ".format(current_value))
+
+    if ARGS_KEY_INPUT not in args:
+        print ('Input must be specified')
+        exit(3)
+    elif ARGS_KEY_OUTPUT not in args:
+        print ('Output must be specified')
+        exit(3)
+
+    return args
+
+
+args = get_args()
+
+
+def save_data(dataset):
+    datafile = args[ARGS_KEY_DATA_FILE]
+
+    fw = open(datafile, 'wb')
+    pickle.dump(dataset, open(datafile, "wb"))
+    fw.close()
+
+
+def load_data():
+    datafile = args[ARGS_KEY_DATA_FILE]
+
+    if not os.path.exists(datafile):
+        save_data({
+            DATA_KEY_LAST_FHASH: -1,
+            DATA_KEY_LAST_BACKUP_FILE: '',
+            DATA_KEY_LAST_FILE_COUNT: 0
+        })
+    return pickle.load(open(datafile, "rb"))
+
+
+def get_all_file_paths(paths):
+    file_paths = []
+
+    for p in paths:
+        if os.path.isfile(p):
+            file_paths.append(p)
+        elif os.path.isdir(p):
+            for root, directories, files in os.walk(p):
+                for filename in files:
+                    filepath = os.path.join(root, filename)
+                    file_paths.append(filepath)
+        else:
+            if os.path.exists(p):
+                print ("File {} is a special file (socket, FIFO, device file)".format(p))
+
+    return file_paths
+
+
+def archive_files(paths, zip_name):
+    files = get_all_file_paths(paths)
+
+    with zipfile.ZipFile(zip_name, 'w') as zip_file:
+        for f in files:
+            zip_file.write(f)
+
+
+def calculate_md5(paths):
+    files = get_all_file_paths(paths)
+    hash_md5 = hashlib.md5()
+
+    for p in files:
+        if os.path.isfile(p):
+            with open(p, "rb") as f:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hash_md5.update(chunk)
+
+    return hash_md5.hexdigest()
+
+
+def create_filename_with_timestamp(filename):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    dir = os.path.dirname(filename)
+    base = os.path.basename(filename)
+    parts = os.path.splitext(base)
+    filenamebase = parts[0]
+    ext = ''
+    if len(parts) > 1:
+        ext = parts[1]
+    targetfilename = filenamebase + '-' + timestr + ext
+    return os.path.join(dir, targetfilename)
+
+
+print ('')
+print ('----------------------------------------------')
+print ('Starting programm with args: {}'.format(args))
+
+data = load_data()
+
+print ('Last data: {}'.format(data))
+print ('----------------------------------------------')
+print ('')
+
+all_files = get_all_file_paths(args[ARGS_KEY_INPUT])
+files_hash = calculate_md5(all_files)
+
+if files_hash != data[DATA_KEY_LAST_FHASH] or \
+        not os.path.exists(data[DATA_KEY_LAST_BACKUP_FILE]) \
+        or data[DATA_KEY_LAST_FILE_COUNT] != len(all_files):
+    target_file = create_filename_with_timestamp(args[ARGS_KEY_OUTPUT])
+    print ('Archiving paths {} to {}'.format(args[ARGS_KEY_INPUT], target_file))
+    archive_files(all_files, target_file)
+    data[DATA_KEY_LAST_FHASH] = files_hash
+    data[DATA_KEY_LAST_BACKUP_FILE] = target_file
+    data[DATA_KEY_LAST_FILE_COUNT] = len(all_files)
+    save_data(data)
+else:
+    print ('Nothing changed, no need to backup')
