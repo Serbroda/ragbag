32315f19db1ae7277ca3e42a1adb7d4b30078a30
Danny Rottstegge <danny@rottstegge.net>
2021-04-10 16:43:43 +0200

    Improve backup script

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+FILENAME_PREFIX=backup
+
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
@@ -16,6 +18,16 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    -n|--name)
+    FILENAME_PREFIX="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -y|--yes)
+    yn="y"
+    shift # past argument
+    shift # past value
+    ;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
     shift # past argument
@@ -25,25 +37,50 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 NOW=$(date +'%Y-%m-%d_%H%M%S')
-FILENAME=backup_$NOW.tar.gz
+FILENAME="$FILENAME_PREFIX"_"$NOW".tar.gz
 
-echo Backup
-echo ---------------
 
 if [ -z "$INPUT_DIR" ]
 then
-    read -p 'Source Directory: ' INPUT_DIR
+    read -p 'Source directory: ' INPUT_DIR
+else
+    echo "Source directory: $INPUT_DIR"
 fi
 
 if [ -z "$OUT_DIR" ]
 then
-    read -p 'Ouput Directory: ' OUT_DIR
+    read -p 'Ouput directory: ' OUT_DIR
+else
+    echo "Ouput directory: $OUT_DIR"
 fi
 
-echo $INPUT_DIR
-echo $OUT_DIR
-echo $FILENAME
+echo
+
+if [ -z "$yn" ]
+then
+    while true; do
+        echo Do you want to start the backup with following options?
+        echo -----------------------------------------------------------
+        echo "- Source directory to created backup for:" $INPUT_DIR
+        echo "- Target backup file:" "$OUT_DIR/$FILENAME"
+        echo 
+        read -p "Start backup (y/n): " yn
+
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+
+echo
+echo "Starting backup $INPUT_DIR -> $OUT_DIR/$FILENAME ..."
+echo
 
 mkdir -p $OUT_DIR
 
-tar -cvpzf "$OUT_DIR/$FILENAME" -C $INPUT_DIR .
+#tar -cvpzf "$OUT_DIR/$FILENAME" -C $INPUT_DIR .
+
+echo 
+echo Backup sucessfully created
